002903fde7bd2b6bae8f36257a2bda2da1b3caf4
Davide De Rosa <keeshux@gmail.com>
2014-12-05 17:54:48 +0100

    Cap transaction size

--- WaSPV/Sources/Core/WSTransaction.m
+++ WaSPV/Sources/Core/WSTransaction.m
@@ -77,6 +77,11 @@ static NSUInteger WSTransactionEstimatedSize(NSOrderedSet *inputs, NSOrderedSet 
         self.lockTime = lockTime;
 
         WSBuffer *buffer = [self toBuffer];
+        if (buffer.length > WSTransactionMaxSize) {
+            WSErrorSet(error, WSErrorCodeInvalidTransaction, @"Transaction is too big (size: %u > %u)", buffer.length, WSTransactionMaxSize);
+            return nil;
+        }
+
         self.txId = [buffer computeHash256];
         self.txIdPrefix = *(NSUInteger *)self.txId.bytes;
         self.size = buffer.length;
