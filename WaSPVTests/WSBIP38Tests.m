c655023ff8c3728f4fc09f924d703dc1e75029eb
Davide De Rosa <keeshux@gmail.com>
2014-12-07 20:22:14 +0100

    BIP38 tests stub

---
+++ WaSPVTests/WSBIP38Tests.m
@@ -0,0 +1,28 @@
+//
+//  WSBIP38Tests.m
+//  WaSPV
+//
+//  Created by Davide De Rosa on 07/12/14.
+//  Copyright (c) 2014 Davide De Rosa. All rights reserved.
+//
+
+#import "XCTestCase+WaSPV.h"
+#import "WSBIP38.h"
+
+@interface WSBIP38Tests : XCTestCase
+
+@end
+
+@implementation WSBIP38Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+@end
