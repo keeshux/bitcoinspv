524145275a3a9cb2119f306501a76e046c202d11
Davide De Rosa <keeshux@gmail.com>
2014-12-07 20:19:57 +0100

    BIP38 stubs

---
+++ WaSPV/Sources/BIPS/WSBIP38.h
@@ -0,0 +1,44 @@
+//
+//  WSBIP38.h
+//  WaSPV
+//
+//  Created by Davide De Rosa on 07/12/14.
+//  Copyright (c) 2014 Davide De Rosa. All rights reserved.
+//
+//  http://github.com/keeshux
+//  http://twitter.com/keeshux
+//  http://davidederosa.com
+//
+//  This file is part of WaSPV.
+//
+//  WaSPV is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  WaSPV is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with WaSPV.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "WSKey.h"
+
+@interface WSBIP38Key : NSObject
+
+- (instancetype)initWithKey:(WSKey *)key passphrase:(NSString *)passphrase;
+
+@end
+
+#pragma mark -
+
+@interface WSKey (BIP38)
+
+- (instancetype)initWithBIP38Key:(WSBIP38Key *)key passphrase:(NSString *)passphrase;
+
+@end
