keeshux/bitcoinspv

commit 039d8ff18c9d4bad3a4ba1e0ddd582b509fa0bb3

Made estimated tx size a global function

WaSPV/Sources/Core/WSTransaction.m
@@ -425,41 +425,3 @@ static NSUInteger WSTransactionEstimatedSize(NSOrderedSet *inputs, NSOrderedSet 
 }
 
 @end
-
-static NSUInteger WSTransactionEstimatedSize(NSOrderedSet *inputs, NSOrderedSet *outputs, BOOL simulatingSignatures)
-{
-    NSUInteger size = 0;
-    
-    size += sizeof(uint32_t); // version
-    
-    size += WSBufferVarIntSize(inputs.count);
-    if (simulatingSignatures) {
-        size += inputs.count * WSTransactionInputTypicalSize;
-    }
-    else {
-        for (id<WSTransactionInput> input in inputs) {
-            size += WSTransactionOutPointSize;
-            const NSUInteger scriptSize = [input.script estimatedSize];
-            size += WSBufferVarIntSize(scriptSize);
-            size += scriptSize;
-            size += sizeof(uint32_t); // sequence
-        }
-    }
-    
-    size += WSBufferVarIntSize(outputs.count);
-    if (simulatingSignatures) {
-        size += outputs.count * WSTransactionOutputTypicalSize;
-    }
-    else {
-        for (WSTransactionOutput *output in outputs) {
-            size += sizeof(uint64_t); // value
-            const NSUInteger scriptSize = [output.script estimatedSize];
-            size += WSBufferVarIntSize(scriptSize);
-            size += scriptSize;
-        }
-    }
-    
-    size += sizeof(uint32_t); // lockTime
-    
-    return size;
-}

WaSPV/Sources/Global/WSBitcoin.h
@@ -81,6 +81,7 @@ extern const uint64_t           WSTransactionFreeMaxSize;
 extern const uint64_t           WSTransactionFreeMinPriority;
 
 NSUInteger WSTransactionTypicalSize(NSUInteger numberOfInputs, NSUInteger numberOfOutputs);
+NSUInteger WSTransactionEstimatedSize(NSOrderedSet *inputs, NSOrderedSet *outputs, BOOL simulatingSignatures);
 
 typedef enum {
     WSTransactionSigHash_ALL            = 0x00000001U

WaSPV/Sources/Global/WSBitcoin.m
@@ -26,6 +26,9 @@
 //
 
 #import "WSBitcoin.h"
+#import "WSTransactionInput.h"
+#import "WSTransactionOutput.h"
+#import "WSScript.h"
 
 //
 // https://en.bitcoin.it/wiki/Base58Check#Base58_symbol_chart
@@ -108,4 +111,42 @@ NSUInteger WSTransactionTypicalSize(NSUInteger numberOfInputs, NSUInteger number
 {
     // version + locktime + ins/outs
     return (8 + numberOfInputs * WSTransactionInputTypicalSize + numberOfOutputs * WSTransactionOutputTypicalSize);
+}
+
+NSUInteger WSTransactionEstimatedSize(NSOrderedSet *inputs, NSOrderedSet *outputs, BOOL simulatingSignatures)
+{
+    NSUInteger size = 0;
+    
+    size += sizeof(uint32_t); // version
+    
+    size += WSBufferVarIntSize(inputs.count);
+    if (simulatingSignatures) {
+        size += inputs.count * WSTransactionInputTypicalSize;
+    }
+    else {
+        for (id<WSTransactionInput> input in inputs) {
+            size += WSTransactionOutPointSize;
+            const NSUInteger scriptSize = [input.script estimatedSize];
+            size += WSBufferVarIntSize(scriptSize);
+            size += scriptSize;
+            size += sizeof(uint32_t); // sequence
+        }
+    }
+    
+    size += WSBufferVarIntSize(outputs.count);
+    if (simulatingSignatures) {
+        size += outputs.count * WSTransactionOutputTypicalSize;
+    }
+    else {
+        for (WSTransactionOutput *output in outputs) {
+            size += sizeof(uint64_t); // value
+            const NSUInteger scriptSize = [output.script estimatedSize];
+            size += WSBufferVarIntSize(scriptSize);
+            size += scriptSize;
+        }
+    }
+    
+    size += sizeof(uint32_t); // lockTime
+    
+    return size;
 }

commit a3096d22177959e90317c79177336acd4660d7ed

Fixed old declaration
Broken in 039d8ff18c9d4bad3a4ba1e0ddd582b509fa0bb3

WaSPV/Sources/Core/WSTransaction.m
@@ -39,8 +39,6 @@
 #import "WSMacros.h"
 #import "WSErrors.h"
 
-static NSUInteger WSTransactionEstimatedSize(NSOrderedSet *inputs, NSOrderedSet *outputs, BOOL simulatingSignatures);
-
 @interface WSSignedTransaction ()
 
 @property (nonatomic, assign) uint32_t version;