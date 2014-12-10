72308d367fcac4353c0a6d25617559b919a393f3
Davide De Rosa <keeshux@gmail.com>
2014-12-10 20:05:23 +0100

    Test all out values at once

--- WaSPVTests/WSScriptTests.m
+++ WaSPVTests/WSScriptTests.m
@@ -265,7 +265,38 @@
     }
 }
 
-- (void)testPubKeysFromMultisigRedeem
+- (void)testComponentsFromMultiSig
+{
+    NSString *expScriptHex = @"0048304502200187af928e9d155c4b1ac9c1c9118153239aba76774f775d7c1f9c3e106ff33c0221008822b0f658edec22274d0b6ae9de10ebf2da06b1bbdaaba4e50eb078f39e3d78014730440220795f0f4f5941a77ae032ecb9e33753788d7eb5cb0c78d805575d6b00a1d9bfed02203e1f4ad9332d1416ae01e27038e945bc9db59c732728a383a6f1ed2fb99da7a4014cc952410491bba2510912a5bd37da1fb5b1673010e43d2c6d812c514e91bfa9f2eb129e1c183329db55bd868e209aac2fbc02cb33d98fe74bf23f0c235d6126b1d8334f864104865c40293a680cb9c020e7b1e106d8c1916d3cef99aa431a56d253e69256dac09ef122b1a986818a7cb624532f062c1d1f8722084861c5c3291ccffef4ec687441048d2455d2403e08708fc1f556002f1b6cd83f992d085097f9974ab08a28838f07896fbab08f39495e15fa6fad6edbfb1e754e35fa1c7844c41f322a1863d4621353ae";
+
+    NSArray *expSignatures = @[@"304502200187af928e9d155c4b1ac9c1c9118153239aba76774f775d7c1f9c3e106ff33c0221008822b0f658edec22274d0b6ae9de10ebf2da06b1bbdaaba4e50eb078f39e3d7801",
+                               @"30440220795f0f4f5941a77ae032ecb9e33753788d7eb5cb0c78d805575d6b00a1d9bfed02203e1f4ad9332d1416ae01e27038e945bc9db59c732728a383a6f1ed2fb99da7a401"];
+
+    NSArray *expPubKeys = @[@"0491bba2510912a5bd37da1fb5b1673010e43d2c6d812c514e91bfa9f2eb129e1c183329db55bd868e209aac2fbc02cb33d98fe74bf23f0c235d6126b1d8334f86",
+                            @"04865c40293a680cb9c020e7b1e106d8c1916d3cef99aa431a56d253e69256dac09ef122b1a986818a7cb624532f062c1d1f8722084861c5c3291ccffef4ec6874",
+                            @"048d2455d2403e08708fc1f556002f1b6cd83f992d085097f9974ab08a28838f07896fbab08f39495e15fa6fad6edbfb1e754e35fa1c7844c41f322a1863d46213"];
+
+    WSScript *script = WSScriptFromHex(expScriptHex);
+    NSArray *signatures;
+    NSArray *pubKeys;
+    WSScript *redeemScript;
+    XCTAssertTrue([script isScriptMultiSigWithSignatures:&signatures publicKeys:&pubKeys redeemScript:&redeemScript]);
+    XCTAssertEqual(signatures.count, expSignatures.count);
+    XCTAssertEqual(pubKeys.count, expPubKeys.count);
+
+    DDLogInfo(@"Signatures: %@", signatures);
+    DDLogInfo(@"Public keys: %@", pubKeys);
+    DDLogInfo(@"Redeem script: %@", redeemScript);
+
+    for (NSData *s in signatures) {
+        XCTAssertEqualObjects(s, [expSignatures[[signatures indexOfObject:s]] dataFromHex]);
+    }
+    for (WSPublicKey *pk in pubKeys) {
+        XCTAssertEqualObjects(pk, WSPublicKeyFromHex(expPubKeys[[pubKeys indexOfObject:pk]]));
+    }
+}
+
+- (void)testPubKeysFromMultiSigRedeem
 {
     WSParametersSetCurrentType(WSParametersTypeMain);
 
