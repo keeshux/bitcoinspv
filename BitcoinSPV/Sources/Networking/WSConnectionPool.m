cdd3cfa71f7f4c7e923a22ef4b7a39697ab50c26
Davide De Rosa <keeshux@gmail.com>
2015-07-05 21:40:20 +0200

    Redundant method, already in protocol

--- BitcoinSPV/Sources/Networking/WSConnectionPool.m
+++ BitcoinSPV/Sources/Networking/WSConnectionPool.m
@@ -39,8 +39,6 @@
 @property (nonatomic, weak) id<WSConnectionHandlerDelegate> delegate;
 
 - (instancetype)initWithParameters:(id<WSParameters>)parameters host:(NSString *)host port:(uint16_t)port processor:(id<WSConnectionProcessor>)processor;
-- (id<WSConnectionProcessor>)processor;
-
 - (void)connectWithTimeout:(NSTimeInterval)timeout error:(NSError **)error;
 
 @end
