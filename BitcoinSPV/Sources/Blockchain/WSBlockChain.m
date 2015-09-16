84745b984d28b57631a53b3e3e9ee34c1dc665f3
Davide De Rosa <keeshux@gmail.com>
2015-09-16 08:48:04 +0200

    Note about slow Core Data loading

--- BitcoinSPV/Sources/Blockchain/WSBlockChain.m
+++ BitcoinSPV/Sources/Blockchain/WSBlockChain.m
@@ -416,6 +416,8 @@
     WSExceptionCheckIllegal(manager);
     
     [self.store truncate];
+    
+#warning FIXME: load from Core Data is very, very slow
 
     __block NSArray *blockEntities = nil;
     [manager.context performBlockAndWait:^{
